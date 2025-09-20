dba9f1ae15699af43641e670e6b5e9d8d1e9c816
Igor Gurchenko <igorivanov122@gmail.com>
2025-09-20 15:55:01 +0300

    Added views.py and urls

--- network/views.py
+++ network/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
-# Create your views here.
+from .models import NetworkLink
+from .serializers import NetworkLinkSerializer
+from .permissions import IsActiveStaff
+
+
+class NetworkLinkViewSet(viewsets.ModelViewSet):
+    """
+    API эндпоинт для работы со звеньями сети.
+    Предоставляет полный CRUD.
+    Возможна фильтрация по стране (?country=НАЗВАНИЕ_СТРАНЫ).
+    """
+    queryset = NetworkLink.objects.all()
+    serializer_class = NetworkLinkSerializer
+    permission_classes = [IsActiveStaff]
+
+    # Подключаем бэкенд для фильтрации
+    filter_backends = [DjangoFilterBackend]
+    # Указываем поля для фильтрации
+    filterset_fields = ['country']
